25ac55af20d6ce1881e82c3bdb534fef2f168fd5
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-08-28 16:32:30 +0200

    Completed exercise 3: Test of addition of two strings.

--- test_calculator.py
+++ test_calculator.py
@@ -5,3 +5,6 @@ def test_add_exercise1():
 
 def test_add_exercise2():
     assert abs(calculator.add(0.1, 0.2) - 0.3) < 1e-12
+
+def test_add_exercise3():
+    assert calculator.add("Hello ", "World") == "Hello World"
