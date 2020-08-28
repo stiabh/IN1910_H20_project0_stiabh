8dd8214c19c0f51fb049573e3a019f42c5398a42
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-08-28 17:02:12 +0200

    Added function factorial, test passed

--- calculator.py
+++ calculator.py
@@ -1,2 +1,16 @@
 def add(x, y):
     return x + y
+
+def factorial(N):
+    if type(N) != int:
+        raise ValueError("Not defined")
+    else:
+        if N < 0:
+            raise ValueError("Not defined")
+        elif N == 0:
+            return 1
+        else:
+            prod = 1
+            for n in range(1, N+1):
+                prod *= n
+    return prod
